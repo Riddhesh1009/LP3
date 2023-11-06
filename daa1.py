2f5d84af2edeb8324611e0466bca9e643f8e05b8
Riddhesh H Mali <74883681+Riddhesh1009@users.noreply.github.com>
2023-11-06 09:18:02 +0530

    Update daa1.py

--- daa1.py
+++ daa1.py
@@ -20,3 +20,23 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+#non recursive
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < n:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+
+# Example usage:
+result = fibonacci(10)
+print(result) 
